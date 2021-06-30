9e2b4fa3c954b993a5561e610f8e6a2b9a511cd4
Danil Serobabin <serobabin.danil@gmail.com>
2021-06-30 22:14:26 +0300

    add interactive communication with the user

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+const name = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    return userName;
+};
+export default name;

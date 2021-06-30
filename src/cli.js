03e0d9257e1b8c26cbc38fe8493cbd3943e62208
Danil Serobabin <serobabin.danil@gmail.com>
2021-06-30 22:35:24 +0300

    add Eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const name = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return userName;
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
 };
+
 export default name;

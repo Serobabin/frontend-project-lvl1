239ff26d1946ed2f3a08903d39977c46181c83a1
Danil Serobabin <serobabin.danil@gmail.com>
2021-07-11 11:04:55 +0300

    fix progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,32 +2,23 @@ import readlineSync from 'readline-sync';
 import getRandomNum from '../RandomNum.js';
 
 const progression = () => {
-  const lengthOfProgression = getRandomNum(6, 10);
+  const lengthOfProgression = 10;
   const stepOfProgression = getRandomNum(1, 10);
   const startOfProgression = getRandomNum(1, 25);
-  const hiddenElement = getRandomNum(0, lengthOfProgression - 1);
-  let question = '';
-  let answer;
+  const hiddenElement = getRandomNum(0, 9);
   let y = 0;
-  for (let i = 0; i <= lengthOfProgression; i += 1) {
-    if (i === hiddenElement) {
-      if (i === 0) {
-        y += startOfProgression;
-        answer = y;
-        question += '..';
-      } else {
-        y += stepOfProgression;
-        answer = y;
-        question += ' ..';
-      }
-    } else if (i === 0 && y === 0) {
-      y += startOfProgression;
-      question += ` ${y}`;
+  const result = [];
+  for (let i = 0; i < lengthOfProgression; i += 1) {
+    if (i === 0) {
+      result.push(startOfProgression);
     } else {
-      y += stepOfProgression;
-      question += ` ${y}`;
+      y = result[i - 1] + stepOfProgression;
+      result.push(y);
     }
   }
+  let answer = result[hiddenElement];
+  result[hiddenElement] = '..';
+  const question = `${result[0]} ${result[1]} ${result[2]} ${result[3]} ${result[4]} ${result[5]} ${result[6]} ${result[7]} ${result[8]} ${result[9]}`;
   const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
   answer = String(answer);
   return (userAnswer !== answer) ? [false, userAnswer, answer] : [true, userAnswer, answer];
